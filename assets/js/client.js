49d3d2fc3dd07090c619766ce3c48b0538ef3117
Deynner Sanabria <rojasdeynner8@gmail.com>
2023-07-19 17:07:46 -0500

    Using async/await and try/catch to handle the response of the promise

--- assets/js/client.js
+++ assets/js/client.js
@@ -3,11 +3,19 @@ const diceButton = document.querySelector('.dice');
 const adviceId = document.querySelector('.adviceId__id');
 const adviceText = document.querySelector('.adviceText');
 
+
+const newAdvice = async () =>{
+    try {
+        const advice =  await (module.generateAdivce())    
+        adviceId.textContent = advice.slip.id;
+        adviceText.textContent = `"${advice.slip.advice}"`;              
+    } catch (error) {
+        alert("An error has occurred");
+    }
+}
+
 diceButton.addEventListener('click',() => {
-    module.generateAdivce().then( (response)=> {
-        adviceId.textContent = response.slip.id;
-        adviceText.textContent = `"${response.slip.advice}"`;              
-    });
+    newAdvice();
 })
 
 
